266ff0e2ecc23ac10e50b3397318060a872cb947
reputationrepeater <rubelsorder394@gmail.com>
2023-12-02 14:32:13 +0600

    Veriable

---
+++ veriable.c
@@ -0,0 +1,32 @@
+
+#include<stdio.h>
+
+int main()
+
+{
+    //Here we have discussed the veriable topic and take a value in veriable and scanf and printf
+
+    /*
+    int age;
+    printf("Enter age ");
+    scanf("%d",&age);
+    printf("Here is the printed age : %d ",age); */
+
+    /*char charecter ;
+    printf("Enter the char ");
+    scanf("%c",&charecter);
+    printf("Here is the main value: %c",charecter); */
+
+    int a, b;
+
+    printf("Enter value a : ");
+    scanf("%d",&a);
+
+    printf("Enter value b : ");
+    scanf("%d",&b);
+
+    int sum = a+b;
+
+    printf("The total sum is : %d", sum);
+
+}
