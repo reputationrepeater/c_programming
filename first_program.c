652b92fdb73966e9f93117e21f7745b676ab1b97
reputationrepeater <rubelsorder394@gmail.com>
2023-12-07 13:52:59 +0600

    Practice_one_file

--- first_program.c
+++ first_program.c
@@ -4,5 +4,7 @@ int main()
 
 {
     printf("hellow");
+
+
 }
 
---
+++ p_1.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+
+
+//Write a program to calculate area of square
+int main()
+
+{   int side;
+    printf("Write down the side: ");
+    scanf("%d",&side);
+    printf("The total square of side is: %d", side * side );
+}
