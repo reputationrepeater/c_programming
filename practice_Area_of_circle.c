aaebd23f57453b5ba0533d6bd39231650abdce45
reputationrepeater <rubelsorder394@gmail.com>
2023-12-13 16:11:33 +0600

    Practice_Circle

---
+++ practice_Area_of_circle.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+int main()
+{
+
+    float side;
+    printf("Please enter the side of circle:  ");
+    scanf("%f",&side);
+
+    printf("The area of circle is : %f", 3.1416*(side * side));
+}
+
