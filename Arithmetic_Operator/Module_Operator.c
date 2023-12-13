53a17d663f82874574396ec0c59ad7088efe1689
reputationrepeater <rubelsorder394@gmail.com>
2023-12-13 18:07:12 +0600

    abc

---
+++ Arithmetic_Operator/Module_Operator.c
@@ -0,0 +1,8 @@
+
+#include<stdio.h>
+
+int main()
+{
+    printf("Print the value of module is : %d\n", 5%2);
+    printf("Print the value of module is : %d\n", 10%2);
+}
---
+++ Arithmetic_Operator/Type_Conversiton.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+#include<math.h>
+
+int main()
+{
+    printf("Here is the example of type conversion: : %d\n", 4*4);
+    printf("Here is the example of type conversion: : %d\n", 4*4.0);
+
+}
