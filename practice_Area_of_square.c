c16ae66204d888d18a757cc6745cba9d76368217
reputationrepeater <rubelsorder394@gmail.com>
2023-12-13 14:29:39 +0600

    Practice_area

---
+++ practice_Area_of_square.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+//Area of side
+int main()
+{
+    int side;
+
+    printf("Please enter the side ");
+    scanf("%d",&side);
+
+    printf("The area of square is : %d", side * side );
+    return 0;
+}
